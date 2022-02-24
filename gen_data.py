53a430c7569a6092ba033696b9b9868620018fce
Dominik <cogentdom@gmail.com>
2022-02-23 23:16:45 -0800

    Update to data and redeploying to cloud run

--- gen_data.py
+++ gen_data.py
@@ -1,9 +1,13 @@
 from main import db
 db.create_all()
 from main import Drink
-drink = Drink(name="Grape Soda", description="Taste like grape")
+drink = Drink(name="Soda", description="Taste like grape")
 db.session.add(drink)
 db.session.commit()
-db.session.add(Drink(name='Cherry', description='Tastes like Cola'))
+db.session.add(Drink(name='Water', description='The healthy option'))
+db.session.commit()
+db.session.add(Drink(name='Beer', description='Im guessing it is the weekend'))
+db.session.commit()
+db.session.add(Drink(name='Milk', description='Cows go Moo'))
 db.session.commit()
 exit()
